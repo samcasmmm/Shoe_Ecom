17fa0646bdcfadc988e40d89239b767ffc89291c
Sameer Bagwan <sameerbagwan132@gmail.com>
2022-05-09 19:58:05 +0530

    Final_v2

---
+++ client frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="justify-content-center d-flex">
+        <div className="card-name">
+          <img
+            alt="paypal"
+            src="https://pbs.twimg.com/media/EfTZlEnWAAMn1lX.png"
+          />
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
